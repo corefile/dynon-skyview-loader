f77478fa36b2c179b1564ae2b4338c95ba77e5ba
Paul Huckaby <luap71@yahoo.com>
2021-05-21 14:07:30 -0700

    Update skyview-loader.sh

--- skyview-loader.sh
+++ skyview-loader.sh
@@ -13,10 +13,13 @@ function backup_usb_drive() {
 }
 
 function sectional_update() {
-    echo ""
-	echo "Backing up USB Drive to local folder (dynon: "
-    rsync -r 
-	echo ""
+  echo "Enter this month's prefix number (4 digits): "
+read PREFIX
+
+echo "Enter this month's password: "
+read PASSWORD
+
+wget -P $TMP_DIR --no-clobber http://data.seattleavionics.com/OEM/Generic/$PREFIX/$PREFIX.SEC.MultiDiskImg.zip
 }
 
 function all_charts_update() {
@@ -26,11 +29,20 @@ function all_charts_update() {
 	echo ""
 }
 
-function dynon_software_update() {
-    echo ""
-	echo "Backing up USB Drive to local folder (dynon: "
-    rsync -r 
-	echo ""
+function dynon_aviation_obstacles_db() {
+echo "Downloading Dynon Aviation and Obstacles Databases..."
+software_ten_path=$(curl -s https://www.dynoncertified.com/software-updates-single.php | grep -m 1 HDX1100 | sed -n 's/^.*href="\(.*.duc\)".*$/\1/p')
+software_ten_filename=$(echo $software_ten_path | sed -n 's/^.*\/\(.*.duc\)$/\1/p')
+software_ten_uri=$(echo "https://www.dynoncertified.com/$software_ten_path")
+wget -P $TMP_DIR --no-clobber $software_ten_uri
+}
+
+function skyview_software_update() {
+echo "Downloading latest Skyview software updates..."
+software_ten_path=$(curl -s https://www.dynoncertified.com/software-updates-single.php | grep -m 1 HDX1100 | sed -n 's/^.*href="\(.*.duc\)".*$/\1/p')
+software_ten_filename=$(echo $software_ten_path | sed -n 's/^.*\/\(.*.duc\)$/\1/p')
+software_ten_uri=$(echo "https://www.dynoncertified.com/$software_ten_path")
+wget -P $TMP_DIR --no-clobber $software_ten_uri
 }
 
 function all_above() {
@@ -39,7 +51,8 @@ function all_above() {
     backup_usb_drive
     sectional_update
     all_charts_update
-    dynon_software_update
+    dynon_aviation_obstacles_db
+    skyview_software_update
 }
 
 ##
@@ -68,8 +81,9 @@ $(BoldTitle 'Dynon Skyview')
 $(ColorGreen '1)') Backup USB Drive
 $(ColorGreen '2)') Sectionals Update
 $(ColorGreen '3)') All Charts Update
-$(ColorGreen '4)') Dynon Software Update
-$(ColorGreen '5)') All Above
+$(ColorGreen '4)') Dynon Aviation & Obstacles DB
+$(ColorGreen '5)') Skyview Software Update
+$(ColorGreen '6)') All Above
 $(ColorGreen '0)') Exit
 $(ColorBlue 'Choose an option:') "
         read a
@@ -77,8 +91,9 @@ $(ColorBlue 'Choose an option:') "
 	        1) backup_usb_drive ; menu ;;
 	        2) sectional_update ; menu ;;
 	        3) all_charts_update ; menu ;;
-	        4) dynon_software_update ; menu ;;
-	        5) all_above ; menu ;;
+		4) dynon_aviation_obstacles_db ; menu ;;
+	        5) skyview_software_update ; menu ;;
+	        6) all_above ; menu ;;
 		0) exit 0 ;;
 		*) echo -e $red"Wrong option."$clear; WrongCommand;;
         esac
